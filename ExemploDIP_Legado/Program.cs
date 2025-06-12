5d1ae7268dcaa552acacf321f0d787af90d59825
GUILHERME FIUZA NOLASCO DE CAMPOS <guilherme_f_campos@estudante.sesisenai.org.br>
2025-06-12 20:47:39 -0300

    commit inicial

---
+++ ExemploDIP_Legado/Program.cs
@@ -0,0 +1,21 @@
+﻿public enum State{On, Off}
+class Lamp(State State)
+{
+    public void Operate()
+    {
+        State = State == State.On ? State.Off : State.On;
+        Console.WriteLine("Luz " + (State == State.On ? "Ligada" : "Desligada"));
+    }
+}
+class Switch
+{
+    private Lamp lamp;
+    public Switch(Lamp device)
+    {
+        this.lamp = device;
+    }
+    public void Press()
+    {
+        lamp.Operate();
+    }
+}
